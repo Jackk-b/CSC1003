7b47eac8bd48f9d64c0c004e4257c58b0032be0d
Jack <jack.burke66@mail.dcu.ie>
2024-09-13 12:00:00 +0100

    Labsheet 1b completed

---
+++ 1b/add-ten-numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+ans = 0
+
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+ans += int(input())
+
+print(ans)
