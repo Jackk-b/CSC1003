6f0ab5624157746ab42d615da21c5df28d08d5d1
Jack <jack.burke66@mail.dcu.ie>
2024-09-13 12:00:00 +0100

    Labsheet 1b completed

---
+++ 1b/add-ten-odd-numbers.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+ans = 0
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+n = int(input())
+r = (n % 2)
+ans += (r * n)
+
+print(ans)
---
+++ 1b/rabbits-everywhere.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+pop = int(input())
+y = int(input())
+
+print(pop * (2 ** y))
